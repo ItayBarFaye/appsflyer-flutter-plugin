79cff246d5ef808bb04791d1ea4cde6c236d3664
ItayBarFaye <106300092+ItayBarFaye@users.noreply.github.com>
2023-08-08 14:38:06 +0300

    Update appsflyer_sdk.dart

--- lib/src/appsflyer_sdk.dart
+++ lib/src/appsflyer_sdk.dart
@@ -79,7 +79,7 @@ class AppsflyerSdk {
       assert(appID != null, "appleAppId is required for iOS apps");
       assert(appID is String);
       RegExp exp = RegExp(r'^\d{8,11}$');
-      assert(exp.hasMatch(appID));
+      //assert(exp.hasMatch(appID));
       validatedOptions[AppsflyerConstants.AF_APP_Id] = appID;
     }
 
@@ -135,7 +135,7 @@ class AppsflyerSdk {
       assert(appID != null, "appleAppId is required for iOS apps");
       assert(appID is String);
       RegExp exp = RegExp(r'^\d{8,11}$');
-      assert(exp.hasMatch(appID));
+      //assert(exp.hasMatch(appID));
       afOptions[AppsflyerConstants.AF_APP_Id] = appID;
     }
 
